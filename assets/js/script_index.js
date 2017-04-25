f423ec480140edb729c08d540b9996f7467fb456
Danny <Danny@calvisitor-10-105-170-219.calvisitor.1918.berkeley.edu>
2017-04-25 12:46:58 -0700

    Fixed Potato Scrolling

--- assets/js/script_index.js
+++ assets/js/script_index.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
 
   $('#carousel-next').click(function() {
     var shift = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (shift  <= -3840) {
+    shift = Math.trunc(shift / 960);
+    if (shift  <= -4) {
       $('#carousel').css('margin-left', "0px");
     } else {
-      $('#carousel').css('margin-left', (shift - 960) + "px"); 
+      $('#carousel').css('margin-left', (shift - 1) * 960 + "px"); 
     };  
   });
 
   $('#carousel-prev').click(function() {
     var shift = parseInt($('#carousel').css('margin-left').replace("px", ""));
-    if (shift >= 0) {
+    shift = Math.trunc(shift / 960);
+    if (shift  >= 0) {
       $('#carousel').css('margin-left', "-3840px");
     } else {
-      $('#carousel').css('margin-left', (shift + 960) + "px");
-    };
+      $('#carousel').css('margin-left', (shift + 1) * 960 + "px"); 
+    }; 
   });
 });
 
