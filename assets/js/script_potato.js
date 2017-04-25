c237611af6c8c8add7e779d06511249d1fd9f1f0
Danny <Danny@calvisitor-10-105-170-219.calvisitor.1918.berkeley.edu>
2017-04-25 14:34:52 -0700

    Sidebar Working

---
+++ assets/js/script_potato.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#foreground").fadeIn("slow");
+});
--- assets/js/script_sidebar.js
+++ assets/js/script_sidebar.js
@@ -13,15 +13,15 @@ $(document).ready(function() {
   // Add the "active" class to the page wrapper
   // Add the "no-scroll" class to the body tag with a delay of 300 ms (code above)
   $( "#sidebar-button" ).click(function() {
-    if ($('#sidebar-container').hasClass('sidebar-active')) {
+    if ($('.sidebar-container').hasClass('sidebar-active')) {
       $( "#sidebar-button" ).removeClass( "button-active" );
-      $( "#sidebar-container" ).removeClass( "sidebar-active" );
+      $( ".sidebar-container" ).removeClass( "sidebar-active" );
       $( "#foreground" ).removeClass( "foreground-active" );
       $( ".page-wrapper" ).removeClass( "wrapper-active" );
     }
     else {
       $( "#sidebar-button" ).addClass( "button-active" );
-      $( "#sidebar-container" ).addClass( "sidebar-active" );
+      $( ".sidebar-container" ).addClass( "sidebar-active" );
       $( "#foreground" ).addClass( "foreground-active" );
       $( ".page-wrapper" ).addClass( "wrapper-active" );
       setTimeout(function() {
@@ -30,10 +30,10 @@ $(document).ready(function() {
   });
 
   //Hiding of the sidebar when the user clicks on the page wrapper
-    $( ".page-wrapper" ).click(function() {
-    if ($('#sidebar-container').hasClass('sidebar-active')) {
+    $( ".page-wrapper, .page" ).click(function() {
+    if ($('.sidebar-container').hasClass('sidebar-active')) {
       $( "#sidebar-button" ).removeClass( "button-active" );
-      $( "#sidebar-container" ).removeClass( "sidebar-active" );
+      $( ".sidebar-container" ).removeClass( "sidebar-active" );
       $( "#foreground" ).removeClass( "foreground-active" );
       $( ".page-wrapper" ).removeClass( "wrapper-active" );
       }
