48bf5a4d4071eda56e9a47ed8dd58005285a7eec
Danny Chu <dannychu@berkeley.edu>
2020-05-30 04:02:21 -0700

    Added Postcard tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
 
-
 });
+
+function mountModule(elem, color, height) {
+	let mount = document.getElementById(elem);
+	let mounted = document.getElementsByClassName("mounted_module");
+
+	Array.prototype.forEach.call(mounted, element => {
+		console.log(element);
+		element.classList.remove("mounted_module");
+		element.classList.add("unmounted_module");
+	});
+
+	mount.classList.remove("unmounted_module");
+	mount.classList.add("mounted_module");
+
+	let folder = document.getElementById("folder-page");
+	folder.style.maxHeight = height;
+	folder.style.backgroundColor = color;
+
+}
+
 
 
